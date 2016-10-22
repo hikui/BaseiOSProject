9a6844ed364a85ff726c0fc66e91cc94b8534453
Heguang Miao <hikuimiao@gmail.com>
2016-10-23 00:38:50 +1100

    Initial Commit

---
+++ MHGBaseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MHGBaseProject
+//
+//  Created by Heguang Miao on 23/10/16.
+//  Copyright © 2016 Heguang Miao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
